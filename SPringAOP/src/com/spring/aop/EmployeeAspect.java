5c4782a65326218a74e54741ca0bf2030dea6a39
Instructor <Instructor@SPANDAN06.igglobal.com>
2018-11-15 15:20:31 +0530

    around

--- SPringAOP/src/com/spring/aop/EmployeeAspect.java
+++ SPringAOP/src/com/spring/aop/EmployeeAspect.java
@@ -3,17 +3,42 @@ package com.spring.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class EmployeeAspect
 {
-	@Before("execution(* com.spring.aop.EmployeeService.getEmployeeByName(..))")
+//	@Before("execution(* com.spring.aop.EmployeeService.getEmployeeByName(..))")
 	public void beforeAdvice(JoinPoint j)
 	{
 		System.out.println("Before execution of jp method ");
 		System.out.println("Name of JoinPOint "+j.getSignature().getName());
 		System.out.println("aRGS :"+Arrays.toString(j.getArgs()));
+	}
+	
+	@Pointcut("execution(* *.*(..))")
+	public void all()
+	{
+		
+	}
+	
+	
+	@Around("all()")
+	public void logAround(ProceedingJoinPoint pj) throws Throwable
+	{
+		
+		
+		System.out.println("Before execution of join point");
+		
+		pj.proceed();
+		
+		System.out.println("After execution of joinpoint.....");
+		
+		
+		
 	}
 }
